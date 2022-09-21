f4985e8f9219a44a7e781ab4ae389b90eeabdd26
kaykayibeh <105394355+kaykayibeh@users.noreply.github.com>
2022-09-20 20:40:55 -0700

    Add passenger counter app via upload

---
+++ Passenger Counter App/index.js
@@ -0,0 +1,24 @@
+let countEl = document.getElementById('count-el')
+let saveEl = document.getElementById('save-el')
+let count = 0
+
+console.log(saveEl)
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+    
+}
+
+function save() {
+    console.log(countEl.innerText)
+}
+
+
+function save() {
+    countStr = count + ' - '
+    saveEl.textContent += countStr
+
+    countEl.textContent = 0
+    count = 0
+}
